a989fd80bfcaa86688e30a48851088b18dec2d02
SnipaGus <matthew.r.l.schultz@gmail.com>
2018-10-21 09:03:57 -0500

    changed updateFire to post

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var db = require('../queries')
 router.get('/api/fires', db.getAllFires);
 router.get('/api/fires/:id', db.getSingleFire);
 router.post('/api/fires', db.createFire);
-router.put('/api/fires/:id', db.updateFire);
+router.post('/api/fires/:id', db.updateFire);
 router.delete('/api/fires/:id', db.removeFire);
 
 
